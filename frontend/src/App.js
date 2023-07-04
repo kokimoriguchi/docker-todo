67c4a4b8b9adab8fa8488bb1b57e834e7cba3219
kokimoriguchi <skoki5231@icloud.com>
2023-07-04 18:40:12 +0900

    別ファイルで作り直す

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Login from "./components/pages/home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>HelloWorld</p>
+        <Login />
       </header>
     </div>
   );
---
+++ frontend/src/components/pages/home.jsx
@@ -0,0 +1,43 @@
+const Login = () => {
+  return (
+    <div>
+      <div className="text-3xl text-center">
+        <h1>Login</h1>
+      </div>
+      <div className="flex justify-center">
+        <form className="w-full max-w-sm">
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Username
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                type="text"
+                placeholder="Username"
+              />
+            </div>
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Password
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                type="password"
+                placeholder="******************"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
