9cd57ed0fd3d19d7158d835741b209873ac47463
kokimoriguchi <skoki5231@icloud.com>
2023-07-05 20:10:10 +0900

    react側からログイン機能完成

--- frontend/src/components/hooks/loginHooks.jsx
+++ frontend/src/components/hooks/loginHooks.jsx
@@ -2,6 +2,6 @@ import baseAxios from "../hooks/axios";
 
 // ログイン用のカスタムフック
 export async function LoginHook(userData) {
-  const response = await baseAxios.post("sign_in", { user: userData });
+  const response = await baseAxios.post("sign_in", userData);
   console.log(response);
 }
